eddaee2ec84f595be57a7923f379db69807732d8
George T. Walters II <walterg2@gmail.com>
2014-03-20 11:50:58 -0400

    More refactoring to make the tests more concise

--- bowling/1/spec/bowlingSpec.js
+++ bowling/1/spec/bowlingSpec.js
@@ -4,26 +4,45 @@ describe("Bowling", function() {
   });
 
   describe("Calculating rolls", function () {
-    var game;
+    var game,
+      context = [
+        {
+          roll : "8",
+          result : 8
+        },
+        {
+          roll : "1",
+          result : 1
+        },
+        {
+          roll : "5",
+          result : 5
+        },
+        {
+          roll : '3',
+          result : 3
+        },
+        {
+          roll : "-",
+          result : 0
+        },
+        {
+          roll : "X",
+          result : 10
+        }
+      ];
 
     beforeEach(function () {
       game = new BowlingGame();
     });
 
-    it("can calculate a score for a miss", function () {
-      expect(game.calculateTotalScore("-")).toBe(0);
-    });
+    context.forEach(function (run) {
+      it("can calculate the score from a roll of " + run.roll + " to be " + run.result, function() {
+        expect(game.calculateTotalScore(run.roll)).toBe(run.result);
+      });
 
-    it("can calculate a score from a strike", function() {
-      expect(game.calculateTotalScore("X")).toBe(10);
-    });
-
-    it("can calculate a score from a non-special roll", function() {
-      expect(game.calculateTotalScore("8")).toBe(8);
-      expect(game.calculateTotalScore("2")).toBe(2);
-      expect(game.calculateTotalScore("4")).toBe(4);
-      expect(game.calculateTotalScore("3")).toBe(3);
     });
 
   });
+  
 });
